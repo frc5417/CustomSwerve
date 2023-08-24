93e365e2247a43d53e2a3c025266f6288fc88fea
InvisibleTiger <jeevanadhya.vinothbabu@gmail.com>
2023-08-24 18:10:17 -0500

    add the camera server and switch it back from test

--- src/main/java/frc/robot/commands/TeleopDrive.java
+++ src/main/java/frc/robot/commands/TeleopDrive.java
@@ -73,7 +73,8 @@ public class TeleopDrive extends CommandBase {
     // if (cnt++ % 50 == 0) 
       // System.out.printf("xVelocity: %f, yVelocity: %f, AngularVel: %f", xVel, yVel, omega);
 
-    m_driveBase.setDriveSpeed(new ChassisSpeeds(0, -5, 0));
+    // m_driveBase.setDriveSpeed(new ChassisSpeeds(0, -5, 0));
+    m_driveBase.setDriveSpeed(new ChassisSpeeds(xVel, yVel, omega));
   }
 
   // Called once the command ends or is interrupted.
